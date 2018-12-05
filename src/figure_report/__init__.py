883ac525e239caf5422068da9ae49c8a1e155699
Stephen Kraemer <stephenkraemer@gmail.com>
2018-12-05 11:43:04 +0100

    update api

--- src/figure_report/__init__.py
+++ src/figure_report/__init__.py
@@ -1,1 +1,7 @@
-from .report import Report
+from figure_report.report import Report
+from figure_report.patterns import (
+    pattern_to_metadata_table,
+    pattern_set_to_metadata_table,
+    copy_report_files_to_report_dir,
+    convert_metadata_table_to_report_json,
+)
